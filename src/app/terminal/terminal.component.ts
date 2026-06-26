824c373bfd47f6f811d932f9355de0145d62ff5d
Thomas VELU <velu.thomas@murena.io>
2026-06-26 16:52:25 +0200

    feat: starting terminal component

---
+++ src/app/terminal/terminal.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-terminal',
+  imports: [],
+  templateUrl: './terminal.component.html',
+  styleUrl: './terminal.component.scss',
+})
+export class Terminal {}
