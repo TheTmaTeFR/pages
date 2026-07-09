175dea10165f1a9ebe8397d3c543d6d779a18887
Thomas VELU <velu.thomas@murena.io>
2026-07-09 11:35:14 +0200

    refactor: remove old project

--- src/app/about-me/data/experience.ts
+++
@@ -1,6 +0,0 @@
-export interface Experience {
-  title: string;
-  location: string;
-  year: string;
-  description: string;
-}
--- src/app/projects/project.ts
+++
@@ -1,8 +0,0 @@
-export interface Project {
-    title: string;
-    description: string;
-    image?: string;
-    year?: number;
-    technologies: string[];
-    link?: string;
-}
